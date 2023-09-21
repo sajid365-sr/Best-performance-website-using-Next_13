bdf4b6564f32c463de41d82f59182b0932779114
sajid365-sr <sajidsorker86@gmail.com>
2023-09-21 09:08:28 +0600

    initial commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,16 @@
+
+import Footer from '@/components/Footer'
+import Navbar from '@/components/Navbar'
+import React from 'react'
+
+const layout = ({children}: {children: React.ReactNode}) => {
+  return (
+    <>
+    <Navbar></Navbar>
+    {children}
+    <Footer></Footer>
+    </>
+  )
+}
+
+export default layout
